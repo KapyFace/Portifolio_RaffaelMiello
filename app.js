69a5bee5284f0a100a8cad4055082c6e9c603b20
Raffael Guideti Miello <126501500+KapyFace@users.noreply.github.com>
2024-05-04 14:49:23 -0300

    Postar arquivos do site

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("aparecer").addEventListener("click", function() {
+    var menu = document.querySelector(".dropdown_menu");
+    menu.style.display = menu.style.display === "none" ? "block" : "none";
+});
